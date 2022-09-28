6af37c80b8ca8ea20174ae95db9c32410f5eff63
Rasim <rasimseferli002@gmail.com>
2022-09-28 10:16:46 +0400

    j

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -1,10 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package javaapplication8;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,8 +18,15 @@ public class JavaApplication8 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
-        File f=new File("forgit.txt");
+        File f = new File("forgit.txt");
+        OutputStream o;
+        try {
+            o = new FileOutputStream(f);
+            o.write(55);
+        } catch (Exception ex) {
+            System.out.println("xeha bash verdi");
+        }
+
     }
-    
+
 }
---
+++ JavaApplication8/src/javaapplication8/Main.java
@@ -0,0 +1,11 @@
+
+package javaapplication8;
+
+
+public class Main {
+    
+    public void test(){
+        System.out.println("hi a am rasim");
+    }
+    
+}
